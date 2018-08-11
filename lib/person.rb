1fd8549c86d8b1c0d505f14e2d3699fc4e7fa491
Mika88 <mikaspivak@gmail.com>
2018-08-11 21:20:05 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size,
                 :wrist_size, :glove_size, :pant_length, :pant_width
@@ -5,5 +6,6 @@ class Person
     attributes = {}
 
     attributes.each {|key, value| self.send(("#{key}="), value)}
+    binding.pry
   end
 end
